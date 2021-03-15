4dd6acf2eb6f1779a23691b4bd412e35fbd6b976
Kashif Ghaffar Asim <kashif.ghaffar100@gmail.com>
2021-03-15 15:24:36 +0500

    Test Case

--- test/controllers/welcomes_controller_test.rb
+++ test/controllers/welcomes_controller_test.rb
@@ -4,4 +4,24 @@ class WelcomesControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
+
+  
+  test "btc conversion" do 
+    get root_url
+    @btc =  Cryptocompare::Price.find('BTC', 'USD')["BTC"]["USD"]
+    assert_response :success 
+  end
+
+  test "Welcome Index" do  
+    get root_url
+    assert_response :success
+  end
+
+
+  test "Conversion Not Eqaul" do 
+    btc = 1245.90
+    @btc =  Cryptocompare::Price.find('BTC', 'USD')["BTC"]["USD"]
+    assert_not_equal btc, @btc 
+  end
+
 end
